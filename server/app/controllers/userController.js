cf0299c580ff71bf61b65a08ba54d309082cdc72
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-08-29 18:26:33 +0200

    debug

--- server/app/controllers/userController.js
+++ server/app/controllers/userController.js
@@ -53,7 +53,7 @@ export const getUserById = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    res.status(200).json(user);
+    res.status(200).json({user});
   } catch (error) {
     res.status(500).json({ error: "Error while fetching user" });
   }
