10ffab42bc8d90e5c1c1ff280491a05079e8a042
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-08-29 13:40:32 +0200

    scss file

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import './normalize.css';
+import './normalize.scss';
 import {Provider} from 'react-redux';
 import {store} from './store/store';
 import './index.scss';
