51cec338fa6fa4f67362b9761f9dbf2b152b7b0e
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-09-04 15:00:33 +0200

    1

--- server/api/api.js
+++ server/api/api.js
@@ -4,33 +4,36 @@ import bodyParser from 'body-parser';
 import userRoute from '../app/routes/userRoute.js'; 
 import productRoute from '../app/routes/productRoute.js';
 import basketRoute from '../app/routes/basketRoute.js';
-import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 const port = process.env.PORT || 3333;
-
-
-
-app.get('/', (req, res) => {
-  const path = `/api/item/${uuidv4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/users', userRoute);
 app.use('/products', productRoute);
 app.use('/basket', basketRoute);
-app.use('/', (req, res) => {
-  res.send('Its wprking')
-})
+
+
+// mongoose.connect('mongodb+srv://dymitrkosow:fullStack4444@cluster0.zkbb11w.mongodb.net/?retryWrites=true&w=majority')
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch((err) => {
+//     console.error('Error connecting to MongoDB:', err);
+//   });
+
+// const start = async() => {
+//   try {
+//     app.listen(port, () => {
+//       console.log(`Server running on http://localhost:${port}`);
+//     });
+//   } catch(e) {
+//     console.log('Error', e)
+//   }
+// };
+
+// start();
 
 export default app;
 
